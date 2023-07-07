d5cd779936d195220ad593178941f90dea1a8e0e
Clark Kampfe <clark.kampfe@gmail.com>
2023-07-06 22:50:43 -0500

    allow elixir 1.14 and later

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WeatherDotGov.MixProject do
     [
       app: :weather_dot_gov,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
