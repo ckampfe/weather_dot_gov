879489a39ffe05f275ee2de1c5c100a3dbb184e8
Clark Kampfe <clark.kampfe@gmail.com>
2023-07-10 19:32:18 -0500

    add package stuff

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule WeatherDotGov.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -26,6 +28,17 @@ defmodule WeatherDotGov.MixProject do
       {:recase, "~> 0.7"},
       {:req, "~> 0.3"},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false}
+    ]
+  end
+
+  def description() do
+    "An API client for weather.gov"
+  end
+
+  def package() do
+    [
+      licenses: ["BSD-3-Clause"],
+      links: %{"GitHub" => "https://github.com/ckampfe/weather_dot_gov"}
     ]
   end
 end
