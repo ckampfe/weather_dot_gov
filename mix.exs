64d4900ac6a39fc194aa39d21f2c9550b0e687c9
Clark Kampfe <clark.kampfe@gmail.com>
2024-06-08 12:49:02 -0500

    0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WeatherDotGov.MixProject do
   def project do
     [
       app: :weather_dot_gov,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: description(),
